f6b6006d465e42a286bfd4ae45b0994ee746f8ac
Abhi04maurya <134590476+Abhi04maurya@users.noreply.github.com>
2025-05-03 08:50:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "3670291"
 api_hash = "1f55b2e9d972c3a5ac3f008c51ed7a4a"
-bot_token = "bot token"
+bot_token = "7837388926:AAEX3vLaOIbrtf8tmWAGH58XqKtI1uF3OPw"
 auth_users = [659925627]
 sudo_users = [659925627]
